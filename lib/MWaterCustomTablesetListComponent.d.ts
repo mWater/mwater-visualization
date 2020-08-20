3b905b8e5535fba5ad754f04b557e88e709b1de8
Clayton Grassick <clayton@claytronics.org>
2020-08-20 07:56:25 -0500

    Build

---
+++ lib/MWaterCustomTablesetListComponent.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="react" />
+import { Schema } from "mwater-expressions";
+/** Searchable list of custom tables */
+export declare const MWaterCustomTablesetListComponent: (props: {
+    apiUrl: string;
+    schema: Schema;
+    client?: string | undefined;
+    /** User id */
+    user?: string | undefined;
+    /** Called with table selected */
+    onChange: (tableId: string | null) => void;
+    extraTables: string[];
+    onExtraTableAdd: (tableId: string) => void;
+    onExtraTableRemove: (tableId: string) => void;
+    /** e.g. "en" */
+    locale?: string | undefined;
+}) => JSX.Element;
---
+++ lib/MWaterMetricsTableListComponent.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="react" />
+import { Schema } from "mwater-expressions";
+/** Searchable list of metric tables */
+export declare const MWaterMetricsTableListComponent: (props: {
+    apiUrl: string;
+    schema: Schema;
+    client?: string | undefined;
+    /** User id */
+    user?: string | undefined;
+    /** Called with table selected */
+    onChange: (tableId: string | null) => void;
+    extraTables: string[];
+    onExtraTableAdd: (tableId: string) => void;
+    onExtraTableRemove: (tableId: string) => void;
+    /** e.g. "en" */
+    locale?: string | undefined;
+}) => JSX.Element;
