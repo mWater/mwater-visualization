8383739a9f1b3f14ffb185aa7ecbd5b65f64f9bb
Clayton Grassick <clayton@claytronics.org>
2022-05-11 08:11:34 -0400

    Fix symbols

--- src/maps/mapSymbols.ts
+++ src/maps/mapSymbols.ts
@@ -1,114 +1,162 @@
+import dot_circle_o from "./symbols/font-awesome/dot-circle-o.png"
+import bullseye from "./symbols/font-awesome/bullseye.png"
+import star from "./symbols/font-awesome/star.png"
+import square from "./symbols/font-awesome/square.png"
+import home from "./symbols/font-awesome/home.png"
+import plus from "./symbols/font-awesome/plus.png"
+import plus_circle from "./symbols/font-awesome/plus-circle.png"
+import plus_square from "./symbols/font-awesome/plus-square.png"
+import asterisk from "./symbols/font-awesome/asterisk.png"
+import mobile from "./symbols/font-awesome/mobile.png"
+import check from "./symbols/font-awesome/check.png"
+import university from "./symbols/font-awesome/university.png"
+import check_circle from "./symbols/font-awesome/check-circle.png"
+import times from "./symbols/font-awesome/times.png"
+import ban from "./symbols/font-awesome/ban.png"
+import crosshairs from "./symbols/font-awesome/crosshairs.png"
+import flask from "./symbols/font-awesome/flask.png"
+import flag from "./symbols/font-awesome/flag.png"
+import info_circle from "./symbols/font-awesome/info-circle.png"
+import exclamation_circle from "./symbols/font-awesome/exclamation-circle.png"
+import exclamation_triangle from "./symbols/font-awesome/exclamation-triangle.png"
+import bell from "./symbols/font-awesome/bell.png"
+import bolt from "./symbols/font-awesome/bolt.png"
+import building from "./symbols/font-awesome/building.png"
+import bus from "./symbols/font-awesome/bus.png"
+import certificate from "./symbols/font-awesome/certificate.png"
+import comment from "./symbols/font-awesome/comment.png"
+import male from "./symbols/font-awesome/male.png"
+import female from "./symbols/font-awesome/female.png"
+import user from "./symbols/font-awesome/user.png"
+import users from "./symbols/font-awesome/users.png"
+import wheelchair from "./symbols/font-awesome/wheelchair.png"
+import h_square from "./symbols/font-awesome/h-square.png"
+import thumbs_up from "./symbols/font-awesome/thumbs-up.png"
+import thumbs_down from "./symbols/font-awesome/thumbs-down.png"
+import ticket from "./symbols/font-awesome/ticket.png"
+import tint from "./symbols/font-awesome/tint.png"
+import times_circle from "./symbols/font-awesome/times-circle.png"
+import tree from "./symbols/font-awesome/tree.png"
+import file from "./symbols/font-awesome/file.png"
+import usd from "./symbols/font-awesome/usd.png"
+import caret_up from "./symbols/font-awesome/caret-up.png"
+import chevron_circle_up from "./symbols/font-awesome/chevron-circle-up.png"
+import chevron_circle_down from "./symbols/font-awesome/chevron-circle-down.png"
+import medkit from "./symbols/font-awesome/medkit.png"
+import cloud from "./symbols/font-awesome/cloud.png"
+import beer from "./symbols/font-awesome/beer.png"
+
 /** Symbols used on the map */
 export const mapSymbols: { value: string; label: string; url: string }[] = [
   {
-    url: require("./symbols/font-awesome/dot-circle-o.png").default,
+    url: dot_circle_o,
     value: "font-awesome/dot-circle-o",
     label: "Dot circle"
   },
-  { url: require("./symbols/font-awesome/bullseye.png").default, value: "font-awesome/bullseye", label: "Bullseye" },
-  { url: require("./symbols/font-awesome/star.png").default, value: "font-awesome/star", label: "Star" },
-  { url: require("./symbols/font-awesome/square.png").default, value: "font-awesome/square", label: "Square" },
-  { url: require("./symbols/font-awesome/home.png").default, value: "font-awesome/home", label: "Home" },
-  { url: require("./symbols/font-awesome/plus.png").default, value: "font-awesome/plus", label: "Plus" },
+  { url: bullseye, value: "font-awesome/bullseye", label: "Bullseye" },
+  { url: star, value: "font-awesome/star", label: "Star" },
+  { url: square, value: "font-awesome/square", label: "Square" },
+  { url: home, value: "font-awesome/home", label: "Home" },
+  { url: plus, value: "font-awesome/plus", label: "Plus" },
   {
-    url: require("./symbols/font-awesome/plus-circle.png").default,
+    url: plus_circle,
     value: "font-awesome/plus-circle",
     label: "Plus Circle"
   },
   {
-    url: require("./symbols/font-awesome/plus-square.png").default,
+    url: plus_square,
     value: "font-awesome/plus-square",
     label: "Plus Square"
   },
-  { url: require("./symbols/font-awesome/asterisk.png").default, value: "font-awesome/asterisk", label: "Asterisk" },
-  { url: require("./symbols/font-awesome/mobile.png").default, value: "font-awesome/mobile", label: "Mobile" },
-  { url: require("./symbols/font-awesome/check.png").default, value: "font-awesome/check", label: "Check" },
+  { url: asterisk, value: "font-awesome/asterisk", label: "Asterisk" },
+  { url: mobile, value: "font-awesome/mobile", label: "Mobile" },
+  { url: check, value: "font-awesome/check", label: "Check" },
   {
-    url: require("./symbols/font-awesome/university.png").default,
+    url: university,
     value: "font-awesome/university",
     label: "Institution"
   },
   {
-    url: require("./symbols/font-awesome/check-circle.png").default,
+    url: check_circle,
     value: "font-awesome/check-circle",
     label: "Check Circle"
   },
-  { url: require("./symbols/font-awesome/times.png").default, value: "font-awesome/times", label: "Removed" },
-  { url: require("./symbols/font-awesome/ban.png").default, value: "font-awesome/ban", label: "Ban" },
+  { url: times, value: "font-awesome/times", label: "Removed" },
+  { url: ban, value: "font-awesome/ban", label: "Ban" },
   {
-    url: require("./symbols/font-awesome/crosshairs.png").default,
+    url: crosshairs,
     value: "font-awesome/crosshairs",
     label: "Crosshairs"
   },
-  { url: require("./symbols/font-awesome/flask.png").default, value: "font-awesome/flask", label: "Flask" },
-  { url: require("./symbols/font-awesome/flag.png").default, value: "font-awesome/flag", label: "Flag" },
+  { url: flask, value: "font-awesome/flask", label: "Flask" },
+  { url: flag, value: "font-awesome/flag", label: "Flag" },
   {
-    url: require("./symbols/font-awesome/info-circle.png").default,
+    url: info_circle,
     value: "font-awesome/info-circle",
     label: "Info Circle"
   },
   {
-    url: require("./symbols/font-awesome/exclamation-circle.png").default,
+    url: exclamation_circle,
     value: "font-awesome/exclamation-circle",
     label: "Exclamation Circle"
   },
   {
-    url: require("./symbols/font-awesome/exclamation-triangle.png").default,
+    url: exclamation_triangle,
     value: "font-awesome/exclamation-triangle",
     label: "Exclamation Triangle"
   },
-  { url: require("./symbols/font-awesome/bell.png").default, value: "font-awesome/bell", label: "Bell" },
-  { url: require("./symbols/font-awesome/bolt.png").default, value: "font-awesome/bolt", label: "Bolt" },
-  { url: require("./symbols/font-awesome/building.png").default, value: "font-awesome/building", label: "Building" },
-  { url: require("./symbols/font-awesome/bus.png").default, value: "font-awesome/bus", label: "Bus" },
+  { url: bell, value: "font-awesome/bell", label: "Bell" },
+  { url: bolt, value: "font-awesome/bolt", label: "Bolt" },
+  { url: building, value: "font-awesome/building", label: "Building" },
+  { url: bus, value: "font-awesome/bus", label: "Bus" },
   {
-    url: require("./symbols/font-awesome/certificate.png").default,
+    url: certificate,
     value: "font-awesome/certificate",
     label: "Certificate"
   },
-  { url: require("./symbols/font-awesome/comment.png").default, value: "font-awesome/comment", label: "Comment" },
-  { url: require("./symbols/font-awesome/male.png").default, value: "font-awesome/male", label: "Male" },
-  { url: require("./symbols/font-awesome/female.png").default, value: "font-awesome/female", label: "Female" },
-  { url: require("./symbols/font-awesome/user.png").default, value: "font-awesome/user", label: "Person" },
-  { url: require("./symbols/font-awesome/users.png").default, value: "font-awesome/users", label: "Group" },
+  { url: comment, value: "font-awesome/comment", label: "Comment" },
+  { url: male, value: "font-awesome/male", label: "Male" },
+  { url: female, value: "font-awesome/female", label: "Female" },
+  { url: user, value: "font-awesome/user", label: "Person" },
+  { url: users, value: "font-awesome/users", label: "Group" },
   {
-    url: require("./symbols/font-awesome/wheelchair.png").default,
+    url: wheelchair,
     value: "font-awesome/wheelchair",
     label: "Wheelchair"
   },
   {
-    url: require("./symbols/font-awesome/h-square.png").default,
+    url: h_square,
     value: "font-awesome/h-square",
     label: "Hospital Symbol"
   },
-  { url: require("./symbols/font-awesome/thumbs-up.png").default, value: "font-awesome/thumbs-up", label: "Thumbs Up" },
+  { url: thumbs_up, value: "font-awesome/thumbs-up", label: "Thumbs Up" },
   {
-    url: require("./symbols/font-awesome/thumbs-down.png").default,
+    url: thumbs_down,
     value: "font-awesome/thumbs-down",
     label: "Thumbs Down"
   },
-  { url: require("./symbols/font-awesome/ticket.png").default, value: "font-awesome/ticket", label: "Ticket" },
-  { url: require("./symbols/font-awesome/tint.png").default, value: "font-awesome/tint", label: "Tint" },
+  { url: ticket, value: "font-awesome/ticket", label: "Ticket" },
+  { url: tint, value: "font-awesome/tint", label: "Tint" },
   {
-    url: require("./symbols/font-awesome/times-circle.png").default,
+    url: times_circle,
     value: "font-awesome/times-circle",
     label: "Times Circle"
   },
-  { url: require("./symbols/font-awesome/tree.png").default, value: "font-awesome/tree", label: "Tree" },
-  { url: require("./symbols/font-awesome/file.png").default, value: "font-awesome/file", label: "File" },
-  { url: require("./symbols/font-awesome/usd.png").default, value: "font-awesome/usd", label: "USD" },
-  { url: require("./symbols/font-awesome/caret-up.png").default, value: "font-awesome/caret-up", label: "Caret Up" },
+  { url: tree, value: "font-awesome/tree", label: "Tree" },
+  { url: file, value: "font-awesome/file", label: "File" },
+  { url: usd, value: "font-awesome/usd", label: "USD" },
+  { url: caret_up, value: "font-awesome/caret-up", label: "Caret Up" },
   {
-    url: require("./symbols/font-awesome/chevron-circle-up.png").default,
+    url: chevron_circle_up,
     value: "font-awesome/chevron-circle-up",
     label: "Chevron Up"
   },
   {
-    url: require("./symbols/font-awesome/chevron-circle-down.png").default,
+    url: chevron_circle_down,
     value: "font-awesome/chevron-circle-down",
     label: "Chevron Down"
   },
-  { url: require("./symbols/font-awesome/medkit.png").default, value: "font-awesome/medkit", label: "Medkit" },
-  { url: require("./symbols/font-awesome/cloud.png").default, value: "font-awesome/cloud", label: "Cloud" },
-  { url: require("./symbols/font-awesome/beer.png").default, value: "font-awesome/beer", label: "Cup" }
+  { url: medkit, value: "font-awesome/medkit", label: "Medkit" },
+  { url: cloud, value: "font-awesome/cloud", label: "Cloud" },
+  { url: beer, value: "font-awesome/beer", label: "Cup" }
 ]
